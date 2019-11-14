b6d94098629cbb21699b782be95538712dbee2c7
Zacck Osiemo <coderv63@gmail.com>
2019-11-14 08:06:43 +0200

    Use line oriented I/O to sum numbers from a file

--- src/hello.cpp
+++ src/hello.cpp
@@ -2,17 +2,19 @@
 #include <fstream>
 #include "precisionexample.h"
 #include "showfilecontents.h"
-
-
+#include <string>
 /* Prototypes */
 void runPrecision();
 void runFileReader();
+void sumFileNumbers();
 
 int main() {
     std::cout << "Precision Examples" << std::endl;
     runPrecision();
     std::cout << std::endl << std::endl;
     runFileReader();
+    std::cout << std::endl << std::endl;
+    sumFileNumbers();
     return 0;
 }
 
@@ -30,11 +32,25 @@ void runPrecision() {
 
 void runFileReader() {
     std::ifstream infile;
-    promptForFile(infile, "Input file: ");
+    promptForFile(infile, "Input file: \n");
     while(true){
         int ch = infile.get();
         if(ch == EOF) break;
         std::cout.put(ch);
     }
     infile.close();
+}
+
+void sumFileNumbers(){
+    std::ifstream infile;
+    promptForFile(infile, "Input file with numbers \n");
+    int total = 0;
+    while(true){
+        std::string line;
+        getline(infile, line);
+        if (infile.fail()) break;
+        total += stringToInt(line);
+    }
+    infile.close();
+    std::cout << "The sum is " << total  << std::endl;
 }
