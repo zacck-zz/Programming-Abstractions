36c2772c250510f262f9b1abcda58691c8fa064c
Zacck Osiemo <coderv63@gmail.com>
2020-01-14 05:16:40 +0200

    Build an interface and implementation for the Pointy class

---
+++ src/pointy.cpp
@@ -0,0 +1,55 @@
+/*
+ * File: pointy.cpp
+ * ------------
+ * Implementation for the pointy class
+ */
+#include <string>
+#include "strlib.h"
+#include "pointy.h"
+
+using namespace std;
+
+
+/*
+ * Constructors
+ * ------------
+ * The constructors initialize the instance variables x and y. In the second
+ * form of the constructor, the parameter names are xc and yc to avoid the problem
+ * of shadowing the instance variables.
+ */
+Pointy::Pointy() {
+    x = 0;
+    y = 0;
+}
+
+Pointy::Pointy(int xc, int yc) {
+    x = xc;
+    y = yc;
+}
+
+
+/*
+ * Getters
+ * --------
+ * The getters return the value of the corresponding instance variable.
+ * No setters are provided to ensure than Pointy objects are immutable.
+ */
+int Pointy::getX() {
+    return x;
+}
+
+int Pointy::getY() {
+    return y;
+}
+
+
+/*
+ * toString
+ * --------
+ * The implementation of toString uses the integerToString function
+ * from the strlib.h interface.
+ */
+string Pointy::toString() {
+    return "(" + integerToString(x) + "," + integerToString(y) + ")";
+}
+
---
+++ src/pointy.h
@@ -0,0 +1,49 @@
+/*
+ * File: Pointy.h
+ * --------------
+ * This interface exports the Point class, which represents a point on a two dimensional integer grid
+ */
+
+#ifndef POINTY_H
+#define POINTY_H
+
+#include <string>
+class Pointy{
+public:
+/*
+ * Constructor: Pointy
+ * Usage: Pointy origin;
+ *        Pointy pt(xc, yc);
+ * -------------------------
+ * Crearws a Pointy object. The default constructor sets the coordinatwe to 0;
+ * the second form sets the coordinate to xc and yc
+ */
+    Pointy();
+    Pointy(int xc, int yc);
+
+/*
+ * Methods: getX, getY
+ * Usage : int x = pt.getX();
+ *         int y = pt.getY();
+ * --------------------------
+ * These methods returns the x and y coordinates of a point
+ */
+
+    int getX();
+    int getY();
+
+/*
+ * Method: toString
+ * Usage: string str = pt.toString();
+ * ----------------------------------
+ * Returns a string represantation of the Pointy in the form of "(x,y)".
+ */
+    std::string toString();
+
+private:
+    int x;  // the x coordinate
+    int y;  // the y coordinate
+};
+
+
+#endif // POINTY_H
