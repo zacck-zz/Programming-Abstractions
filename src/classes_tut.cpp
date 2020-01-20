80474f031f3eb50438be9190d8ece21c81a91365
Zacck Osiemo <coderv63@gmail.com>
2020-01-20 18:19:09 +0200

    Test rational class and move main

--- src/classes_tut.cpp
+++ src/classes_tut.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include "directiony.h"
+#include "rational.h"
 
 using namespace std;
 
 
 void cycleDirs();
+void rationalOps();
 
-int main(){
+int classy(){
     cout << "Working with Classes" << endl;
+
+    rationalOps();
+
+    cout << endl;
 
     cycleDirs();
     return 0;
@@ -18,4 +24,16 @@ void cycleDirs(){
     for(Directiony d = NORTH; d <= WEST; d++) {
         cout << d << endl;
     }
+}
+
+
+void rationalOps(){
+    Rational r1 = Rational(1, 2);
+    Rational r2 = Rational(3, 5);
+
+    cout << r2 <<" + " << r1 << " = "<< (r1 +r2) << endl;
+    cout << r1 << " / " << r2 << " = " << (r1 / r2) << endl;
+    cout << r1 << " * " << r2 << " = " << (r1 * r2) << endl;
+    cout << r1 << " - " << r2 << " = " << (r1 - r2) << endl;
+
 }
